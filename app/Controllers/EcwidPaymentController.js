29b85dcd2fbde08a7653b08c65f294a3671bc763
Kirillpryanikov <prianikovkirill@gmail.com>
2018-08-04 15:31:39 +0300

    add yandex payment method for testing

--- app/Controllers/EcwidPaymentController.js
+++ app/Controllers/EcwidPaymentController.js
@@ -24,6 +24,32 @@ class EcwidPaymentController {
 
         var instance = axios.create();
 
+        instance.get('https://payment.yandex.net/api/v3/',{}).then( res=> {
+            console.log(res);
+        }).catch( error => {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log('REEEEESSSSSSSPPPPPOOOONNNSSSSEEE');
+                console.log(error.response);
+                //console.log(error.response.data);
+                Response.send(error.response.data);
+
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log('REEEEEEEEEQQQQQQUUUUUUEEEEEESSSSTTTTTTT');
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('ERRROR MESSSAAAGGEEE');
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        });
+
+        return;
         instance.post('https://payment.yandex.net/api/v3/payments', {
             headers: headers,
             data: {
